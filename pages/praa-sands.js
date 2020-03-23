aa625530286c7db0b68ae80801405e61da873a3e
Harry Mumford <harry_mumford@hotmail.co.uk>
2020-03-23 21:33:02 +0000

    feat: renders date

--- pages/praa-sands.js
+++ pages/praa-sands.js
@@ -1,28 +1,30 @@
 import axios from "axios"
-import react from "react"
+import React from "react"
 
 // ?={timestamp: 1584921600}
 
-class PraaSands extends React.Component {
-  state = {
-    waveData: []
+export default class PraaSands extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      waveData: []
+    }
   }
 
-  getWaveData() {
+  componentDidMount() {
     axios.get('http://magicseaweed.com/api/e872632fcaa41717190e1812a493dc3b/forecast/?spot_id=8')
     .then(response => {
       console.log(response.data)
+      const waveData = response.data[0].timestamp
+      this.setState({ waveData })
     })
   }
 
   render() {
     return (
-      <div>
-        <p>Praa Sands</p>
-        <button onClick={this.getWaveData}>Hello</button>
-      </div>
+      <p>
+        {this.state.waveData}
+      </p>
     )
   }
 }
-
-export default PraaSands
