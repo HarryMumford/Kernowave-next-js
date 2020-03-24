c9d5fc13aafa63b13fd0cc0ba3b48691b4159c5e
Harry Mumford <harry_mumford@hotmail.co.uk>
2020-03-24 17:25:00 +0000

    feat: renders day and average breaking wave height

--- pages/praa-sands.js
+++ pages/praa-sands.js
@@ -7,24 +7,42 @@ export default class PraaSands extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      waveData: []
+      day: [],
+      conditions: {}
     }
   }
 
   componentDidMount() {
     axios.get('http://magicseaweed.com/api/e872632fcaa41717190e1812a493dc3b/forecast/?spot_id=8')
     .then(response => {
-      console.log(response.data)
-      const waveData = response.data[0].timestamp
-      this.setState({ waveData })
+      const data = response.data
+      this.storeDay(data)
+      this.storeConditionsToday(data[0])
     })
+  }
+
+  storeDay(data) {
+    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const day = days[new Date(data[0].timestamp * 1000).getDay() - 1]
+    this.setState({ day: day })
+  }
+
+  storeConditionsToday(data) {
+    console.log(data)
+    const conditions = {
+      day: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][new Date().getDay()-1],
+      swell: `${Math.round((data.swell.maxBreakingHeight - data.swell.minBreakingHeight)*3.28084)}ft`,
+      wind: ``
+    }
+    this.setState({ conditions: conditions })
   }
 
   render() {
     return (
-      <p>
-        {this.state.waveData}
-      </p>
+      <>
+        <a>{this.state.conditions.day}</a>
+        <p>{this.state.conditions.swell}</p>
+      </>
     )
   }
 }
