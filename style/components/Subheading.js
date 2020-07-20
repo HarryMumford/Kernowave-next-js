b2732263c96f3ac2e426e198bb630d032ee6f034
Harry Mumford <harry_mumford@hotmail.co.uk>
2020-07-20 14:30:09 +0100

    style: remove underline

--- style/components/Subheading.js
+++ style/components/Subheading.js
@@ -2,7 +2,6 @@ import styled from "styled-components"
 
 export default styled.h2`
   color: #1b262c;
-  font-family: 'Noto Serif SC', serif;
-  text-decoration: underline;
+  font-family: "Noto Serif SC", serif;
   font-size: 1.2rem;
 `
