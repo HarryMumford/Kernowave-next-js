c6d333e65a8027386edf23414215f2db248242bb
Harry Mumford <harry_mumford@hotmail.co.uk>
2020-08-04 16:39:24 +0100

    refac: extract daily forecast to own component

--- components/slider.js
+++ components/slider.js
@@ -1,15 +1,7 @@
 import Link from "next/link"
 import React from "react"
-import DailyForecast from "../style/components/DailyForecast"
-import SwellText from "../style/components/SwellText"
-import Subheading from "../style/components/Subheading"
 import StyledSlider from "../style/components/StyledSlider"
-import WindSpeedText from "../style/components/WindSpeedText"
-import WindDirectionText from "../style/components/WindDirectionText"
-import StyledFontAwesomeIcon from "../style/components/StyledFontAwesomeIcon"
-import WindConditionsContainer from "../style/components/WindConditionsContainer"
-import { faLocationArrow } from "@fortawesome/free-solid-svg-icons"
-import { faWind } from "@fortawesome/free-solid-svg-icons"
+import DailyForecast from "./DailyForecast"
 
 var settings = {
   slidesToShow: 4,
@@ -39,34 +31,9 @@ export default function Slider(props) {
     <StyledSlider {...settings}>
       {props.forecast.map(dailyForecast => {
         return (
-          <DailyForecast
-            style={{ backgroundColor: "red" }}
-            key={dailyForecast.day}
-          >
-            <Link href="/beach/[id]/[day]">
-              <Subheading>{dailyForecast.day}</Subheading>
-            </Link>
-            <SwellText quality={dailyForecast.quality.overall}>
-              {dailyForecast.waveHeight} ft
-            </SwellText>
-            <WindConditionsContainer>
-              <WindSpeedText windSpeed={dailyForecast.quality.windSpeed}>
-                <StyledFontAwesomeIcon icon={faWind} />
-                {dailyForecast.windSpeed} mph
-              </WindSpeedText>
-              <WindDirectionText
-                windDirection={dailyForecast.quality.windDirection}
-              >
-                <StyledFontAwesomeIcon
-                  icon={faLocationArrow}
-                  transform={{
-                    rotate: dailyForecast.windDirection - 45
-                  }}
-                />
-                {dailyForecast.windRelativeDirection}
-              </WindDirectionText>
-            </WindConditionsContainer>
-          </DailyForecast>
+          <Link href="/beach/[id]/[day]" key={dailyForecast.day}>
+            <DailyForecast dailyForecast={dailyForecast}></DailyForecast>
+          </Link>
         )
       })}
     </StyledSlider>
--- style/components/DailyForecast.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-
-export default styled.div`
-  cursor: pointer;
-  &:hover {
-    background-color: whitesmoke;
-    border-radius: 10%;
-  }
-`
